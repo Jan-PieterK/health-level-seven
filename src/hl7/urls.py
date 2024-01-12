2b605711e0b2bb7270c9c9fa50da72ef9dd5f541
Leendert Visser <leendert@cisolutions.nl>
2024-01-12 20:10:18 +0100

    Lines formated via black

--- src/hl7/urls.py
+++ src/hl7/urls.py
@@ -20,4 +20,6 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT
+    )
